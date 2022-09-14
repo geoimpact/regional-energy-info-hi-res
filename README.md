532af4a418d746447e1292e9c8fabe578d21c3f4
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2022-09-14 11:00:14 +0200

    wip

---
+++ README.md
@@ -0,0 +1,5 @@
+# Uncomfort
+
+A game to make you feel the uncomfort of cultural appropriation.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {useEffect} from "react";
+import {Typography} from "@mui/material";
+
+function App() {
+    useEffect(() => {
+
+    }, [])
+    return (
+        <div id={"App"}>
+            <Typography variant={"h1"}>regional-energy-info-hi-res</Typography>
+            <Typography variant={"body1"}>regional-energy-info-hi-res</Typography>
+        </div>
+    );
+}
+
+export default App;
