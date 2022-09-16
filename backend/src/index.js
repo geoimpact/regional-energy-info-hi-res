a79f3049a449f9005bc1f220af828fafb14f235c
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2022-09-16 16:04:55 +0200

    wip

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const puppeteer = require('puppeteer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    app.use(cors())
+    // parse application/json
+    app.use(bodyParser.json())
+    app.post('/', async (req, res) => {
+        const page = await browser.newPage();
+        await page.goto(req.body.url);
+        const extractedText = await page.$eval('*', (el) => el.innerText);
+        const htmlText = await page.$eval("body", (el) => {
+            return el.innerHTML
+        })
+        res.send({
+            meta: req.body,
+            text: extractedText,
+            html: htmlText
+        });
+    })
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+})();
---
+++ src/components/Router.js
@@ -0,0 +1,135 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import districts from "../data/districts.json";
+import providers from "../data/providers.json";
+import axios from "axios";
+import {Button, Card, Container, TextareaAutosize, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+
+const googleApiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+const googleSearchCx = process.env.REACT_APP_GOOGLE_SEARCH_ENGINE_ID;
+const backendUrl = process.env.REACT_APP_BACKEND_1;
+
+
+providers = providers
+    .filter(p => p.Link && p.Link.trim().length > 0)
+    .filter(p => p.Link.startsWith("https"));
+
+async function requestScraping(url, body){
+    return axios({
+        method: "post",
+        url: url,
+        data: body
+    });
+}
+
+function Scrape() {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+
+    }, [])
+    const scan = async () => {
+        let districtsTemp = districts.Daten.slice(0, 5);
+        for (let i = 0; i < districtsTemp.length; i++) {
+            let data = districtsTemp[i];
+            console.log("scanning", data.Gemeindename);
+            let searchText = `${data.Gemeindename} Energie`;
+            let response = await axios({
+                method: "get",
+                url: `https://customsearch.googleapis.com/customsearch/v1?q=${searchText}&cx=${googleSearchCx}&key=${googleApiKey}`,
+                headers: {
+                    "Accept": "application/json"
+                }
+            });
+            let newData = {
+                ...(data || {})
+            };
+            newData[`${data.Gemeindename}`] = response.data;
+            setData(newData);
+        }
+    }
+    const scrapeProviders = async () => {
+        for(let i = 0; i < providers.length; i++){
+            let provider = providers[i];
+            console.log(`scraping provider ${provider.Name}: ${provider.Link}`, provider);
+            await requestScraping(backendUrl, {
+                url: provider.Link
+            }).catch(e => e.response);
+        }
+    }
+    return (
+        <Card id={"App"} maxWidth={"xl"} style={{
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center"
+        }}>
+            <Typography variant={"h6"}>Service Providers</Typography>
+            <TextareaAutosize
+                maxRows={10}
+                style={{
+                    minWidth: "400px",
+                    minHeight: "200px",
+                    maxHeight: "300px"
+                }}>
+                {providers.map(p => p.Link).join("\n")}
+            </TextareaAutosize>
+            <Typography variant={"h6"}>Municipalities</Typography>
+            <TextareaAutosize
+                maxRows={10}
+                style={{
+                    minWidth: "400px",
+                    minHeight: "200px",
+                    maxHeight: "300px"
+                }}>
+                Municipalities
+            </TextareaAutosize>
+            <Container maxWidth="xl" style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <Button onClick={async () => {
+                    await scrapeProviders();
+                }}>
+                    scan
+                </Button>
+            </Container>
+        </Card>
+    );
+}
+
+function Util() {
+    return (
+        <div>
+            Util
+        </div>
+    )
+}
+
+function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+
+export default function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/util" element={<Util/>}/>
+                <Route path="/scrape" element={<Scrape/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
